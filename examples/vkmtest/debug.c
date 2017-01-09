3dda96e850bcf61e009d74849e4e60c0a35b9862
blue42u <blue42u@users.noreply.github.com>
2017-01-09 07:32:10 -0600

    Updated copyright year

--- examples/vkmtest/debug.c
+++ examples/vkmtest/debug.c
@@ -1,5 +1,5 @@
 /**************************************************************************
-   Copyright 2016 Jonathon Anderson
+   Copyright 2016-2017 Jonathon Anderson
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
