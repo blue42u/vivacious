e16222350b09c24d6f96fcaf7f83e559737aef1c
blue42u <blue42u@users.noreply.github.com>
2018-01-29 20:27:46 -0600

    Updated the VkShader system to the ShaderBank.

--- apis/shaderbank.lua
+++ apis/shaderbank.lua
@@ -14,41 +14,30 @@
    limitations under the License.
 --]========================================================================]
 
-local std = require 'standard'
-local vk = require 'vulkan'
-local vks = {api=std.precompound{
-	shortname = 'VkS',
-	longname = 'Vulkan Patchwork Shader Constructor',
-	doc = [[A manager and stitcher for merging multiple shaders into one.]],
-}}
+vk = require 'vulkan'
 
-vks.Bank = std.handle{doc=[[A storage for shaders that will be merged.]]}
-vks.Component = std.handle{doc=[[A reference to one of the shaders in a Bank]]}
+ShaderBank = {doc = [[
+	A storage for shader components, which are shaders that have been loaded and
+	are ready to be merged into larger shaders that cumulate the effects of
+	the individual parts.
+]]}
+local sb = ShaderBank
 
-vks.api.v0_1_2.createBank = std.func{
-	doc = [[ Create a new empty Bank. ]],
-	returns = vks.Bank,
-}
+sb.Component = {doc = [[A shader that has been loaded into a Bank.]]}
 
-vks.api.v0_1_2.destroyBank = std.func{
-	doc = [[ Destroy a Bank and all Components contained within. ]],
-	vks.Bank,
-}
-
-vks.api.v0_1_2.loadShader = std.func{
+sb.v0_1_2.load = {
 	doc = [[
-		Load a shader module into a Bank, which is then available
-		for use by later calls to `construct`.
+		Load a shader into the Bank, for later use in constructing new shaders.
+		May return NULL if something goes wrong.
 	]],
-	returns = vks.Component,
-	vks.Bank, vk.ShaderModuleCreateInfo,
+	returns = {sb.Component}, {'smci', vk.Vk.ShaderModuleCreateInfo},
 }
 
-vks.api.v0_1_2.construct = std.func{
+sb.v0_1_2.construct = {
 	doc = [[
-		Stitch together multiple shader modules into one, creating a
-		If the shader code given to `loadShader` is valid, then the
-		resulting code given to Vulkan will be valid.
+		Construct a new shader that cumulates the effects of the previously
+		loaded shaders given in <components>. The following is the old docs;
+		they assume a SPIR-V based system and should be rewritten at some point.
 
 		All instructions and operands in the original code will be
 		present in the code given to Vulkan, with the following
@@ -76,9 +65,6 @@ vks.api.v0_1_2.construct = std.func{
 		If there does not exist an M for a particular set of EPs,
 		another set can be chosen as stated above.
 	]],
-	returns = {vk.ShaderModule, vk.Result, main=2},
-	vks.Bank, vk.Device, std.array{vks.Component},
+	returns = {vk.Device.ShaderModule, vk.Vk.Result},
+	{'dev', vk.Device}, {'components', array{sb.Component}},
 }
-
-vks.api = std.compound(vks.api)
-return vks
